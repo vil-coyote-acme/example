f5ebd0efd38bca12e53464c55c63d35967f91a19
jeromedoucet <jerdct@gmail.com>
2016-03-05 18:15:50 +0100

    add a simple function with a simple test

---
+++ example.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+}
+
+func Something() string {
+	return "something"
+}
---
+++ example_test.go
@@ -0,0 +1,11 @@
+package main_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"example"
+)
+
+func Test_Something(t *testing.T) {
+	assert.Equal(t, "something", main.Something())
+}
